a2238cabf02a8e839d209fb3985faf2eb01be015
whn <whn@utry.cn>
2019-08-09 00:03:18 +0800

    补充

---
+++ src/views/articleComponent.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="gc-article">
+        <button @click="goback" class="btn goback-btn">返回上一层</button>
+        <div class="article-list">
+            <minelist :parentData="frompage" ref="minelistRef"></minelist>
+        </div>
+    </div>
+</template>
+
+<script>
+    import '../assets/scss/article.scss'
+    import minelist from './minelist'
+    export default {
+        name: "articleComponent",
+        components:{
+            minelist
+        },
+        data(){
+            return{
+                frompage:''
+            }
+        },
+        created(){
+            this.frompage = this.$router.currentRoute.params.name
+        },
+        mounted(){
+            this.$refs.minelistRef.getlist()
+        },
+        methods:{
+            goback(){
+                this.$router.go(-1);
+            },
+        },
+    }
+</script>
